f04f55e9fb2a6198d4f77844bc04e99ddd6d42e3
Peter <Xtra55778@gmail.com>
2023-02-26 18:44:24 +0300

    first commit

---
+++ git_test.py
@@ -0,0 +1,22 @@
+# from math import pi
+#
+# def generatoe1():
+#     equator = 40075
+#     meridian = 40009
+#
+#     radiusToUp =int(meridian /( 2* pi))
+#     radiusToLeft = int (equator / (2 * pi))
+#
+#     a = (radiusToUp + (radiusToLeft - radiusToUp) / 20 * i for i in range(0, 20))
+#
+#     a = list(a)
+#
+#     return a
+#
+# print(generatoe1())
+
+def summa(a, b):
+    return a + b
+
+
+print(summa(4, 7))
